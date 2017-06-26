9624eacbfe4cb7b43a4e92d7086818b5ef845895
Greg Answer <greganswer@gmail.com>
2017-06-25 23:54:59 -0400

    app complete

--- node/note_app/app.js
+++ node/note_app/app.js
@@ -1,8 +1,32 @@
 const fs = require('fs');
 const _ = require('lodash');
-const argv = require('yargs').argv;
+const yargs = require('yargs');
 
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
 const notes = require('./notes.js');
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions,
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 
 var command = argv._[0];
 

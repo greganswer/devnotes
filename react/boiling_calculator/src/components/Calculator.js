fec5b2ecb91ab97813fa2ad37c8617b949fc7d58
Greg Answer <greganswer@gmail.com>
2017-07-04 20:28:48 -0400

    Boiling point calculator complete

--- react/boiling_calculator/src/components/Calculator.js
+++ react/boiling_calculator/src/components/Calculator.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
 import BoilingVerdict from './BoilingVerdict';
+import TemperatureInput from './TemperatureInput';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = { temperature: '' };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: '', scale: 'c' };
+
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
   }
 
-  handleChange(e) {
-    this.setState({ temperature: e.target.value });
+  handleCelsiusChange(temperature) {
+    this.setState({ scale: 'c', temperature });
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({ scale: 'f', temperature });
   }
 
   render() {
-    let temperature = this.state.temperature;
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius =
+      scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit =
+      scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
 
     return (
-      <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input value={temperature} onChange={this.handleChange} />
-        <BoilingVerdict celsius={parseFloat(temperature)} />
-      </fieldset>
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celsius={parseFloat(celsius)} />
+      </div>
     );
   }
 }
 
 export default Calculator;
+
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return celsius * 9 / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}

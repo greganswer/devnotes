94bb009e2e0bd9187ecd5ad24c1ecfaeae1d9a2e
Greg Answer <greganswer@gmail.com>
2017-07-22 20:56:29 -0400

    Additional files

---
+++ vue/vue_cli/src/ServerStatus.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: 'Critical',
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = 'Normal';
+    },
+  },
+};
+</script>

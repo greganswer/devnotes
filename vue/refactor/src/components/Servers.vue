ca420486c5f3857cd76e54855f9fab08561e3691
Greg Answer <greganswer@gmail.com>
2017-07-22 21:26:16 -0400

    refactor complete

---
+++ vue/refactor/src/components/Servers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+      <li class="list-group-item" v-for="index in 5">
+        Server #{{ index }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>

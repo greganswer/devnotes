0b03c1cde454864967ccb0cec15d9c7671aea833
Greg Answer <greganswer@gmail.com>
2017-07-25 19:47:48 -0400

    Test get request to Twitter

--- node/twitter_bot/index.js
+++ node/twitter_bot/index.js
@@ -8,3 +8,14 @@ var T = new Twit({
   access_token: secrets.access_token,
   access_token_secret: secrets.access_token_secret,
 });
+
+var params = { q: 'banana', count: 3 };
+
+function callback(err, data, response) {
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].text);
+  }
+}
+
+T.get('search/tweets', params, callback);

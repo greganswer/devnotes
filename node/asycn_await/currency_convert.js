f73f2d7e8d836e115c506220165cdd97e4c814c3
Greg Answer <greganswer@gmail.com>
2017-07-29 22:07:43 -0400

    Add error handling

--- node/asycn_await/currency_convert.js
+++ node/asycn_await/currency_convert.js
@@ -3,28 +3,43 @@ const axios = require('axios');
 const getExchangeRate = async (from, to) => {
   from = from.toUpperCase();
   to = to.toUpperCase();
-  const response = await axios.get(`https://api.fixer.io/latest?base=${from}`);
-  return response.data.rates[to];
+
+  try {
+    const response = await axios.get(
+      `https://api.fixer.io/latest?base=${from}`,
+    );
+    const rate = response.data.rates[to];
+    if (!rate) {
+      throw new Error();
+    }
+    return rate;
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}`);
+  }
 };
 
 const getCountries = async currencyCode => {
   currencyCode = currencyCode.toLowerCase();
-  const response = await axios.get(
-    `https://restcountries.eu/rest/v2/currency/${currencyCode}`,
-  );
-  return response.data.map(country => country.name);
+
+  try {
+    const response = await axios.get(
+      `https://restcountries.eu/rest/v2/currency/${currencyCode}`,
+    );
+    return response.data.map(country => country.name);
+  } catch (e) {
+    throw new Error(`Unable to find countries that use ${currencyCode}`);
+  }
 };
 
 const convertCurrency = async (from, to, amount) => {
   const countries = await getCountries(to);
   const rate = await getExchangeRate(from, to);
   const exchangeAmount = rate * amount;
+  const countriesList = countries.join(', ');
 
-  return `$${amount} ${from} is worth $${exchangeAmount} ${to}.You can spend these in the following countries: ${countries.join(
-    ', ',
-  )}`;
+  return `$${amount} ${from} is worth $${exchangeAmount} ${to}.You can spend these in the following countries: ${countriesList}`;
 };
 
-convertCurrency('cad', 'usd', 40)
+convertCurrency('cad', 'xxx', 40)
   .then(output => console.log(output))
-  .catch(e => console.log(e));
+  .catch(e => console.log(e.message));
